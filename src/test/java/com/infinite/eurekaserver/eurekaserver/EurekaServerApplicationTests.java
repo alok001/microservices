60700b461622054fdfefeebe0c4a4a4c4ed57a8d
alok001 <alokmazumdar001@gmail.com>
2021-06-09 17:05:42 +0200

    eureka-server

---
+++ src/test/java/com/infinite/eurekaserver/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinite.eurekaserver.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class EurekaServerApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}
